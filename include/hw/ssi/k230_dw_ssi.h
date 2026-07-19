3b07653da26676bac5340cbb64f5b604d288be93
flamboy <flamboyant.h.01@gmail.com>
2026-07-19 23:04:17 +0800

    hw/ssi: Add K230 DesignWare SSI register model
    Add a SysBus model for the K230 DesignWare SSI controllers.
    
    The model provides the documented register layout, reset values, writable
    masks, FIFO state, chip-select GPIOs, and the MMIO regions used by the
    K230 integration.  It does not instantiate a controller yet.
    
    The machine integration and qtest coverage are introduced by follow-up
    commits.
    
    Signed-off-by: flamboy <flamboyant.h.01@gmail.com>

---
+++ include/hw/ssi/k230_dw_ssi.h
@@ -0,0 +1,39 @@
+/*
+ * K230 DWC SSI compatible SPI/QSPI controller
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_SSI_K230_DW_SSI_H
+#define HW_SSI_K230_DW_SSI_H
+
+#include "hw/core/sysbus.h"
+#include "hw/ssi/ssi.h"
+#include "qemu/fifo32.h"
+#include "qom/object.h"
+
+#define TYPE_K230_DW_SSI "riscv.k230.dw-ssi"
+OBJECT_DECLARE_SIMPLE_TYPE(K230DwSsiState, K230_DW_SSI)
+
+#define K230_DW_SSI_MMIO_SIZE 0x1000
+#define K230_DW_SSI_REGS_SIZE 0x14c
+#define K230_DW_SSI_NUM_REGS \
+    (K230_DW_SSI_REGS_SIZE / sizeof(uint32_t))
+
+struct K230DwSsiState {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mmio;
+    SSIBus *spi;
+    qemu_irq *cs_lines;
+
+    Fifo32 tx_fifo;
+    Fifo32 rx_fifo;
+    uint32_t regs[K230_DW_SSI_NUM_REGS];
+
+    uint32_t num_cs;
+    uint32_t max_lines;
+    int active_cs;
+};
+
+#endif /* HW_SSI_K230_DW_SSI_H */
